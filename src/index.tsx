b00b614ccab1d95611ceef917f433c2a301ae9f4
Konstantin Mazur <incerta.dei@gmail.com>
2020-10-14 15:02:03 +0300

    Clean CRA boilerplate code

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
 import * as serviceWorker from './serviceWorker';
+import Battleship from './components/Battleship'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Battleship />
   </React.StrictMode>,
   document.getElementById('root')
 );
